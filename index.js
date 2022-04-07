1c4e1ab3535bf614707f13b5142c2d34c6768f66
Deven <devenzachuth@gmail.com>
2022-04-07 15:55:54 -0500

    commit tree 3ee8aabf587f115e3b263652d721ab74a1c5f0e9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+
+findMatching = (drivers, name) => {
+    const driver = drivers.filter(drivers => drivers == name)
+    if (driver){
+        return driver
+    }
+}
+
+// fuzzyMatch = (drivers, letters) => {
+//     const firstTwo = drivers.filter(drivers => drivers.indexOf(letters) == letters)
+//         if (firstTwo){
+//             return firstTwo
+//         }
+// }
+
+console.log(drivers)
+console.log(findMatching(drivers, "Bobby"))
